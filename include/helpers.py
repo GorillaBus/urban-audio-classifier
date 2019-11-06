4006e3d24b789022cbeb46abbabdf64e88263422
Eduardo <eduugr@gmail.com>
2019-11-06 14:45:09 +0100

    Recovered confusion matrix ploting function

--- include/helpers.py
+++ include/helpers.py
@@ -272,3 +272,52 @@ def plot_train_history(history):
     plt.figure(num=1, figsize=(10, 6))
     plt.xticks(np.arange(0, len(history['accuracy']), 5.0))
     plt.show()
+
+
+def compute_confusion_matrix(y_true, 
+               y_pred, 
+               classes, 
+               normalize=False):
+
+    # Compute confusion matrix
+    cm = metrics.confusion_matrix(y_true, y_pred)
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    return cm
+
+
+# Plots a confussion matrix
+def plot_confusion_matrix(cm,
+                          classes, 
+                          normalized=False, 
+                          title=None, 
+                          cmap=plt.cm.Blues,
+                          size=(10,10)):
+    fig, ax = plt.subplots(figsize=size)
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+
+    # We want to show all ticks...
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           # ... and label them with the respective list entries
+           xticklabels=classes, yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    fmt = '.2f' if normalized else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+
+    fig.tight_layout()
+    plt.show()
